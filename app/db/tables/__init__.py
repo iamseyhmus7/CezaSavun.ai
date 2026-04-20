7e37199e5d5db7443172b2c3e39ed25034e79eb7
iamseyhmus7 <seyhmusok01@gmail.com>
2026-04-20 09:56:07 +0300

    fix(db): Notification modelinin SQLAlchemy Base şemasına kaydı yapıldı

--- app/db/tables/__init__.py
+++ app/db/tables/__init__.py
@@ -2,6 +2,7 @@ from app.db.base import Base
 from app.db.tables.user import User
 from app.db.tables.penalty import Penalty
 from app.db.tables.petition import Petition
+from app.db.tables.notification import Notification
 
 # Bu dosya, alembic veya Base.metadata.create_all() çağrıldığında
 # tüm modellerin memory'ye yüklenmesi için kullanılır.
