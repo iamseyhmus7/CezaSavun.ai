62bdabacc0a379165fb24a2a096b37f2ba3955fc
iamseyhmus7 <seyhmusok01@gmail.com>
2026-04-28 12:28:54 +0300

    fix: app/api/v1/router.py içine yeni config endpoint'i dahil edildi.

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import petitions, auth, ws, notifications, admin
+from app.api.v1.endpoints import petitions, auth, ws, notifications, admin, config
 
 api_router = APIRouter()
+api_router.include_router(config.router, prefix="", tags=["config"])
 api_router.include_router(petitions.router, prefix="/petitions", tags=["petitions"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(ws.router, prefix="/ws", tags=["websocket"])
